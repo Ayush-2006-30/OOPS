00c8b3df60927d9fe67ceb35a2e7b8bf0bfa0537
Ayush-2006-30 <ayushsonar2025.etc@mmcoe.edu.in>
2026-09-23 13:42:57 +0530

    Create Tut2.cpp

---
+++ Tut2.cpp
@@ -0,0 +1,44 @@
+//Find out the sum of even numbers and sum of odd numbers between 1 to 50 using //loops. 
+//Program 2: Sum of even numbers and sum of odd numbers 
+#include <iostream> 
+using namespace std; 
+class Numbersum{ 
+private: 
+ int start, end; 
+ int oddsum = 0; 
+ int evensum = 0; 
+public: 
+ void Range(); 
+ void sum(); 
+ int geteven_sum(); 
+ int getodd_sum(); 
+}; 
+void Numbersum::Range() { 
+ cout << "Enter the starting number: "; 
+ cin >> start; 
+ cout << "Enter the ending number: "; 
+ cin >> end; 
+} 
+void Numbersum::sum() { 
+ for (int i = start; i <= end; i++) { 
+ if (i % 2 == 0) 
+ evensum += i; 
+ else 
+ oddsum += i; 
+ } 
+} 
+int Numbersum::geteven_sum() { 
+ return evensum; 
+}
+int Numbersum::getodd_sum() { 
+ return oddsum; 
+} 
+void display(Numbersum N) { 
+ cout << "Even sum of numbers: " << N.geteven_sum() << endl;  cout << "Odd sum of numbers : " << N.getodd_sum() << endl; } 
+int main() { 
+ Numbersum N; 
+ N.Range(); 
+ N.sum(); 
+ display(N); 
+ return 0; 
+} 
